8f2a9b79e914dd0ea86fc6e8973f34bbfd602485
Ehson_Boboev <ehsonboboev2@gmail.com>
2025-09-20 13:14:17 +0700

    cdd

--- src/main/java/org/example/TodoApp.java
+++ src/main/java/org/example/TodoApp.java
@@ -9,7 +9,7 @@ public class TodoApp {
     public static void main(String[] args) {
 
         TodoList list = new TodoList();
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);///sdfdf
         System.out.println("Simple Todo CLI. Commands: add <task>, remove <index>, list, exit");
 
 
